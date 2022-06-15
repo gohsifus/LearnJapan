ca357670b7bccca7876784226e8a93c9a5ee8ea2
gohsifus <57272115+gohsifus@users.noreply.github.com>
2022-06-15 13:24:58 +0300

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func init(){
-	db, err := sql.Open("mysql", "admin:qawsed345rf@tcp(host.docker.internal:3306)/jpncards")
+	db, err := sql.Open("mysql", "admin:qawsed345rf@tcp(185.189.167.212:3306)/jpncards")
 	if err != nil{
 		panic("Ошибка подключения к базе")
 	}
